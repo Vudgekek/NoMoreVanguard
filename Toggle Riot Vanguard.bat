898542c5e40273cf26abe282e126b72d0b692d13
V <business@vudgekek.com>
2023-01-04 18:14:35 -0500

    Add check for required administrative privileges

--- Toggle Riot Vanguard.bat
+++ Toggle Riot Vanguard.bat
@@ -16,6 +16,7 @@ if exist "vgk.sys" (
     ren vgc.exe vgc.exe.bak
     ren vgtray.exe vgtray.exe.bak
     ren vgrl.dll vgrl.dll.bak
+    ren installer.exe installer.exe.bak
     sc config vgc start= disabled & sc config vgk start= disabled & net stop vgc & net stop vgk & taskkill /IM vgtray.exe
 ) else (
     if exist "vgk.sys.bak" (
@@ -23,6 +24,7 @@ if exist "vgk.sys" (
         ren vgc.exe.bak vgc.exe
         ren vgtray.exe.bak vgtray.exe
         ren vgrl.dll.bak vgrl.dll
+        ren installer.exe.bak installer.exe
         sc config vgc start= demand & sc config vgk start= system & shutdown /r /f /t 00
     ) 
 )
