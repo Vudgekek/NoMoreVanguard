f4f1e2713358900507a08be61d0373516a0ca9ed
V <business@vudgekek.com>
2023-01-04 17:48:04 -0500

    Initial commit

---
+++ Toggle Riot Vanguard.bat
@@ -0,0 +1,28 @@
+@echo off
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    continue
+) else (
+    echo Please run script as administrator!
+    pause 10
+    exit
+)
+
+pushd "C:\Program Files\Riot Vanguard"
+
+if exist "vgk.sys" (
+    ren vgk.sys vgk.sys.bak
+    ren vgc.exe vgc.exe.bak
+    ren vgtray.exe vgtray.exe.bak
+    ren vgrl.dll vgrl.dll.bak
+    sc config vgc start= disabled & sc config vgk start= disabled & net stop vgc & net stop vgk & taskkill /IM vgtray.exe
+) else (
+    if exist "vgk.sys.bak" (
+        ren vgk.sys.bak vgk.sys
+        ren vgc.exe.bak vgc.exe
+        ren vgtray.exe.bak vgtray.exe
+        ren vgrl.dll.bak vgrl.dll
+        sc config vgc start= demand & sc config vgk start= system & shutdown /r /f /t 00
+    ) 
+)
