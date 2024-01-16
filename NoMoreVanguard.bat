ca5654a5d7c4c694a74217de4454c6cd527f7c23
Vudgekek <business@vudgekek.com>
2024-01-16 18:38:54 -0500

    Use for-loop instead of just naming files and delete log files

--- NoMoreVanguard.bat
+++ NoMoreVanguard.bat
@@ -1,15 +1,14 @@
 @echo off
 
-REM Obtain admin perms by calling UAC and creating a temporary VBS script that elevates the batch file
+REM Obtain admin perms by creating a temporary VBS script that calls a UAC prompt
 net session >nul 2>&1
-
 if not %errorlevel% == 0 (
     echo Requesting administrative privileges...
-    echo set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo set UAC = CreateObject^("Shell.Application"^) > "%temp%\UAC.vbs"
     set "params=%*"
-    echo UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\UAC.vbs"
+    "%temp%\UAC.vbs"
+    del "%temp%\UAC.vbs"
     exit /b
 ) else (
     pushd "%cd%"
@@ -20,7 +19,7 @@ set "VANGUARD_DIR=%PROGRAMFILES%\Riot Vanguard"
 
 REM Checks for valid directory
 if not exist "%VANGUARD_DIR%" (
-    echo Vanguard directory is invalid. It is either not at the default location or Vanguard is not installed.
+    echo Vanguard directory is invalid. It is either not at the default location or not installed.
     pause
     exit
 ) 
@@ -29,7 +28,7 @@ REM Changes working directory to Vanguard directory
 pushd "%VANGUARD_DIR%"
 
 REM Checks for conflicting versions of Vanguard, removes outdated version found
-for %%a in ("vgk.sys", "vgc.exe", "vgtray.exe", "vgrl.dll", "installer.exe") do (
+for %%a in ("vgk.sys", "vgc.exe", "vgtray.exe", "vgrl.dll", "installer.exe", "log-uploader.exe") do (
     if exist "%%a" (
         del "%%a.bak" >nul 2>&1
     )
@@ -40,11 +39,11 @@ REM Actual Script
 if exist "vgk.sys" (
     REM Renames various files used by Vanguard to make them temporarily unusable, then stops the services
     echo Disabling Vanguard...
-    ren vgk.sys vgk.sys.bak
-    ren vgc.exe vgc.exe.bak
-    ren vgtray.exe vgtray.exe.bak
-    ren vgrl.dll vgrl.dll.bak
-    ren installer.exe installer.exe.bak
+    for %%a in ("vgk.sys", "vgc.exe", "vgtray.exe", "vgrl.dll", "installer.exe", "log-uploader.exe") do (
+        ren "%%a" "%%a.bak"
+    )
+    pushd "Logs"
+    del "*log"
     sc config vgc start= disabled >nul 2>&1
     sc config vgk start= disabled >nul 2>&1
     net stop vgc >nul 2>&1
@@ -52,11 +51,9 @@ if exist "vgk.sys" (
     taskkill /f /im vgtray.exe >nul 2>&1
 ) else (
     REM Reverts changes made by disable function and reinstates services, then restarts the system after 30 seconds w/ countdown
-    ren vgk.sys.bak vgk.sys
-    ren vgc.exe.bak vgc.exe
-    ren vgtray.exe.bak vgtray.exe
-    ren vgrl.dll.bak vgrl.dll
-    ren installer.exe.bak installer.exe
+    for %%a in ("vgk.sys", "vgc.exe", "vgtray.exe", "vgrl.dll", "installer.exe", "log-uploader.exe") do (
+        ren "%%a.bak" "%%a"
+    )
     sc config vgc start= demand
     sc config vgk start= system
     for /l %%b in (30 -1 1) do (
