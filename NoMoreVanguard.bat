e30415b10533da736f99584c1cac28713ffabee5
Vudgekek <business@vudgekek.com>
2024-01-17 18:22:11 -0500

    Simplify script for-loops

--- NoMoreVanguard.bat
+++ NoMoreVanguard.bat
@@ -10,9 +10,6 @@ if not %errorlevel% == 0 (
     "%temp%\UAC.vbs"
     del "%temp%\UAC.vbs"
     exit /b
-) else (
-    pushd "%cd%"
-    cd /d "%~dp0"
 )
 
 set "VANGUARD_DIR=%PROGRAMFILES%\Riot Vanguard"
@@ -28,7 +25,7 @@ REM Changes working directory to Vanguard directory
 pushd "%VANGUARD_DIR%"
 
 REM Checks for conflicting versions of Vanguard, removes outdated version found
-for %%a in ("vgk.sys", "vgc.exe", "vgtray.exe", "vgrl.dll", "installer.exe", "log-uploader.exe") do (
+for %%a in ("installer.exe", "log-uploader.exe", "vgc.exe", "vgc.ico", "vgk.sys", "vgrl.dll", "vgtray.exe") do (
     if exist "%%a" (
         del "%%a.bak" >nul 2>&1
     )
@@ -36,29 +33,29 @@ for %%a in ("vgk.sys", "vgc.exe", "vgtray.exe", "vgrl.dll", "installer.exe", "lo
 
 :Toggle
 if exist "vgk.sys" (
-    REM Renames various files used by Vanguard to make them temporarily unusable, then stops the services
+    REM Stops Vanguard services, renames key files, and deletes Vanguard logs
     echo Disabling Vanguard...
     sc config vgc start= disabled >nul 2>&1
     sc config vgk start= disabled >nul 2>&1
     net stop vgc >nul 2>&1
     net stop vgk >nul 2>&1
     taskkill /f /im vgtray.exe >nul 2>&1
-    for %%a in ("vgk.sys", "vgc.exe", "vgtray.exe", "vgrl.dll", "installer.exe", "log-uploader.exe") do (
+    for %%a in (*) do (
         ren "%%a" "%%a.bak"
     )
     del /q "Logs"
 ) else (
     REM Reverts changes made by disable function and reinstates services, then restarts the system after 30 seconds w/ countdown
     echo Enabling Vanguard...
-    for %%a in ("vgk.sys", "vgc.exe", "vgtray.exe", "vgrl.dll", "installer.exe", "log-uploader.exe") do (
-        ren "%%a.bak" "%%a"
+    for %%a in (*) do (
+        ren "%%a" "%%~na"
     )
     sc config vgc start= demand
     sc config vgk start= system
     for /l %%b in (30 -1 1) do (
         cls
         echo Restarting in: %%b Seconds - [C]ancel / [R]estart Now
-        for /f "Delims=" %%c in ('Choice /T 1 /N /C:CSRW /D W') do (
+        for /f "delims=" %%c in ('Choice /T 1 /N /C:CSRW /D W') do (
             if %%c==C goto :Toggle
             if %%c==R shutdown /r /f /t 00
         )
